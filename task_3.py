6a33be178712fb233a41ca814b83b873714072b3
sidorkin95 <sidorkin95@gmail.com>
2026-01-16 22:33:25 +0300

    sprint_1

---
+++ task_3.py
@@ -0,0 +1,20 @@
+world_champions = {
+    2002: 'Бразилия',
+    2006: 'Италия',
+    2010: 'Испания',
+    2014: 'Германия',
+    2018: 'Франция',
+}
+
+world_champions[2022] = 'Аргентина'
+for champion in world_champions.keys():
+    print(champion, '-', world_champions[champion])
+    
+country = 'Италия'
+
+if country in world_champions.values():
+
+    print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
+else:
+    print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
+
